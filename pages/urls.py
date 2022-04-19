d3da204c5323510606d6751f847ba111411eba18
RaufMasoumi <raouf.rm93@gmail.com>
2022-04-20 00:45:34 +0430

    pages is done.

--- pages/urls.py
+++ pages/urls.py
@@ -1,25 +1,14 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, hello, IndexView, AboutView, AccountView, CheckoutView, ContenctsView,FaqView, GoodsView, \
-    ItemView, PrivacyPolicyView, ProductListView, SearchResultView, ShoppingCartNullView, ShoppingCartView, StandardFormsView, TermsConditionsView, WishListView
+from .views import HomePageView, AboutPageView, AccountPageView, CheckoutView, ContactsPageView, FaqPageView, \
+    PrivacyPolicyPageView, TermsConditionsPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('hello/', hello),
-    path('index/', IndexView.as_view(), name='index'),
-    path('new-about/', AboutView.as_view(), name='new-about'),
-    path('account/', AccountView.as_view(), name='account'),
+    path('account/', AccountPageView.as_view(), name='account'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('contacts/', ContenctsView.as_view(), name='contacts'),
-    path('faq/', FaqView.as_view(), name='faq'),
-    path('goods/', GoodsView.as_view(), name='goods'),
-    path('item/', ItemView.as_view(), name='item'),
-    path('privacy/', PrivacyPolicyView.as_view(), name='privacy'),
-    path('product/list/', ProductListView.as_view(), name='product_list'),
-    path('search/', SearchResultView.as_view(), name='search_result'),
-    path('cart/', ShoppingCartView.as_view(), name='shopping_cart'),
-    path('cart/null/', ShoppingCartNullView.as_view(), name='shopping_cart_null'),
-    path('standard/forms/', StandardFormsView.as_view(), name='standard_forms'),
-    path('terms/', TermsConditionsView.as_view(), name='terms'),
-    path('wishlist/', WishListView.as_view(), name='wishlist'),
+    path('contacts/', ContactsPageView.as_view(), name='contacts'),
+    path('faq/', FaqPageView.as_view(), name='faq'),
+    path('privacy-policy/', PrivacyPolicyPageView.as_view(), name='privacy_policy'),
+    path('terms-conditions/', TermsConditionsPageView.as_view(), name='terms_conditions'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,63 +6,7 @@ from books.views import PageLocation
 from django.contrib.auth import get_user_model
 # Create your views here.
 
-
 class HomePageView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['search_form'] = BookSearchForm()
-        return context
-
-
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
-
-
-def hello(request):
-    return render(request, 'hello.html')
-
-class AboutView(TemplateView):
-    template_name = 'newtemplates/shop-about.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account'), PageLocation('About', 'about', True)]
-        return context
-
-class AccountView(TemplateView):
-    template_name = 'newtemplates/shop-account.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account', True)]
-        return context
-class CheckoutView(TemplateView):
-    template_name = 'newtemplates/shop-checkout.html'
-
-class ContenctsView(TemplateView):
-    template_name = 'newtemplates/shop-contacts.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account'), 
-            PageLocation('Contacts', 'contacts', True)]
-        return context
-
-class FaqView(TemplateView):
-    template_name = 'newtemplates/shop-faq.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account'), 
-            PageLocation('Frequently Asked Questions', 'faq', True)]
-        return context
-
-class GoodsView(TemplateView):
-    template_name = 'newtemplates/shop-goods-compare.html'
-
-class IndexView(TemplateView):
     template_name = 'newtemplates/shop-index.html'
 
     def get_context_data(self, **kwargs):
@@ -73,10 +17,44 @@ class IndexView(TemplateView):
         context['fast_view_books'] = context['sales'] | context['new_arrivals'] | context['bestsellers']
         return context
 
-class ItemView(TemplateView):
-    template_name = 'newtemplates/shop-item.html'
+class AboutPageView(TemplateView):
+    template_name = 'newtemplates/shop-about.html'
 
-class PrivacyPolicyView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account'), PageLocation('About', 'about', True)]
+        return context
+
+class AccountPageView(TemplateView):
+    template_name = 'newtemplates/shop-account.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account', True)]
+        return context
+
+class CheckoutView(TemplateView):
+    template_name = 'newtemplates/shop-checkout.html'
+
+class ContactsPageView(TemplateView):
+    template_name = 'newtemplates/shop-contacts.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account'), 
+            PageLocation('Contacts', 'contacts', True)]
+        return context
+
+class FaqPageView(TemplateView):
+    template_name = 'newtemplates/shop-faq.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account'), 
+            PageLocation('Frequently Asked Questions', 'faq', True)]
+        return context
+
+class PrivacyPolicyPageView(TemplateView):
     template_name = 'newtemplates/shop-privacy-policy.html'
     
     def get_context_data(self, **kwargs):
@@ -85,22 +63,7 @@ class PrivacyPolicyView(TemplateView):
             PageLocation('Privacy and Policy', 'privacy', True)]
         return context
 
-class ProductListView(TemplateView):
-    template_name = 'newtemplates/shop-product-list.html'
-
-class SearchResultView(TemplateView):
-    template_name = 'newtemplates/shop-search-result.html'
-
-class ShoppingCartView(TemplateView):
-    template_name = 'newtemplates/shop-shopping-cart.html'
-
-class ShoppingCartNullView(TemplateView):
-    template_name = 'newtemplates/shop-shopping-cart-null.html'
-
-class StandardFormsView(TemplateView):
-    template_name = 'newtemplates/shop-standart-forms.html'
-
-class TermsConditionsView(TemplateView):
+class TermsConditionsPageView(TemplateView):
     template_name = 'newtemplates/shop-terms-conditions-page.html'
 
     def get_context_data(self, **kwargs):
@@ -108,6 +71,3 @@ class TermsConditionsView(TemplateView):
         context['page_location_list'] = [PageLocation('Home', 'home'), PageLocation('Account', 'account'), 
             PageLocation('Terms and Conditions', 'terms', True)]
         return context
-
-class WishListView(TemplateView):
-    template_name = 'newtemplates/shop-wishlist.html'
