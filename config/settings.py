65cc0ff60ef7c67f4e89301936de1046ad50c8d8
RaufMasoumi <raouf.rm93@gmail.com>
2022-06-27 12:49:46 +0430

    the project heroku host was added to allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '.herokuapp.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost', '127.0.0.1', 'rauf-book-store-app.herokuapp.com'])
 
 
 # Application definition
